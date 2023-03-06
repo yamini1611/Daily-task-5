30e3c65117b3c9efa0ecc66fd399cc79d52b2ac4
selvaG2 <selvaganapathikg2@gmail.com>
2023-03-06 10:18:02 +0530

    1

---
+++ index.js
@@ -0,0 +1,35 @@
+function Validate() {
+    const userName = document.getElementById("form1Example13").value;
+    const password = document.getElementById("form1Example23").value;
+    const confirmPass = document.getElementById("form1Example24").value;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,15}$/;
+
+
+    console.log(userName);
+
+    if (!userName || !password || !confirmPass)
+    {
+        alert("Fields con`t be empty");
+        return false;
+    }
+    else if (!/^[a-zA-Z\s]+$/.test(userName))
+    {
+        alert("User name should contain only numbers");
+    }
+    else if (password != confirmPass)
+    {
+        alert("Both Password and Confirm password does`t match");
+        return false;
+    }
+    else if (!passwordRegex.test(password))
+    {
+        alert("Password should contain atleast 1 upper case 1 lowercase 1 numberic and 1 letter with min 8 char");
+    }
+    else
+    {
+        alert("Login successfully..!");
+        return true;
+    }
+
+    
+}
